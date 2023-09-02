d8ce44366954aaf68a62c3d691e8f57561b34968
joao guilherme <joaoguilhermehig@gmail.com>
2023-09-02 18:27:55 -0300

    FIREBASE STORAGE COLOCADO E YARN INSTALADO

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyAHU5ZRqkufdSLf1tpZkFFRTNa4ajxlnHg",
+  authDomain: "banco-camera.firebaseapp.com",
+  projectId: "banco-camera",
+  storageBucket: "banco-camera.appspot.com",
+  messagingSenderId: "343361982535",
+  appId: "1:343361982535:web:e32ded712ef201576f6162",
+  measurementId: "G-PWPL28HT61",
 };
 
 const app = initializeApp(firebaseConfig);
